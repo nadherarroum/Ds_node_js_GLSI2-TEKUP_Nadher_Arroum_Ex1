26ddba719421954dc4f98dcfdeab956dd902fe19
Nadher Arroum <nadher.arroum@gmail.com>
2022-03-17 13:25:42 +0100

    Test register functionality

--- index.js
+++ index.js
@@ -5,9 +5,12 @@ const appDebug = require('debug')('app:debug');
 const port = process.env.PORT || 3000;
 
 const admin_route = require('./routers/admin');
+const auth_route = require('./routers/auth');
+
 const app = express();
 
 app.use(express.json());
+app.use('/api/v1/user',auth_route);
 app.use('/api/v1/admin',admin_route);
 
 
