92091df41b5657adb0b2dbd11a5d1f30b5b2824a
Nadher Arroum <nadher.arroum@gmail.com>
2022-03-17 10:32:20 +0100

    Update certif

--- routers/admin.js
+++ routers/admin.js
@@ -35,4 +35,27 @@ router.get('/certif/:title', async(req, res)=>{
     res.send(certif);
 });
 
+// Update certif 
+router.put('/certif/:title',async (req,res)=>{
+    // Find certif
+    let title = req.params.title;
+    let certif = await Certif.find({title : title});
+    // Return 404 if not existing
+    if(!certif)
+        res.status(404).send('The certif you look for is not found !');
+    // Validate
+    let validation_res = certif_validation.validate(req.body);
+    // Return 400 - Bad request if it's invalid
+    if (validation_res.error) {
+        return res.status(400).send(validation_res.error.details[0].message);
+    }
+    // Update certif
+    let cert = new Certif(req.body);
+    cert = await cert.save();
+    // Return the updated certif
+    res.send(cert);
+    
+});
+
+
 module.exports=router;
